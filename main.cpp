88c445fd37691bad97d4bfb1eb6d0db8652a871e
Leeczxc <leeczxc@outlook.com>
2023-05-30 11:13:34 +0800

    json baby

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@
 
 struct JSONobject;
 
+using JSONDict = std::unordered_map<std::string, JSONobject>;
+using JSONList = std::vector<JSONobject>;
+
 struct JSONobject{
     std::variant
     < std:: nullptr_t
@@ -23,11 +26,25 @@ struct JSONobject{
     , int
     , double 
     , std::string
-    , std::vector<JSONobject>
-    , std::unordered_map<std::string, JSONobject>
+    , JSONList
+    , JSONDict
     > inner;
     void do_print() const{
         print(inner);
+    }
+    
+    template<class T>
+    bool is() const{
+        return std::holds_alternative<T>(inner);    
+    }
+
+    template<class T>
+    T const &get() const{
+        return std::get<T>(inner);    
+    }
+    template<class T>
+    T &get() {
+        return std::get<T>(inner);    
     }
 };
     
@@ -60,6 +77,9 @@ std::pair<JSONobject, size_t> parse(std::string_view json){
     if(json.empty()){
         return {JSONobject{std::nullptr_t{}}, 0};
         // int && double
+    }else if(size_t off = json.find_first_not_of(" \n\t\r\v\f\0"); off != 0 && off != json.npos){
+        auto [obj, eaten] = parse(json.substr(off));
+        return {std::move(obj), eaten + off};    
     }else if ('0' < json[0] && json[0] <= '9' || json[0] == '+' || json[0] == '-'){
         //0-9, ., e, -,+
         std::regex num_re{"[+-]?[0-9]+(\\.[0-9]*)?([eE][+-]?[0-9]+)?"};
@@ -119,16 +139,85 @@ std::pair<JSONobject, size_t> parse(std::string_view json){
             }
         }              
         return {JSONobject{std::move(res)}, i};
-    };
+    }else if(json[0] == '{'){
+        std::unordered_map<std::string, JSONobject> res;
+        size_t i;
+        for(i = 1; i < json.size();){
+            if(json[i] == '}'){
+                i += 1;
+                break;
+            }
+            auto[keyobj, keyeaten] = parse(json.substr(i));
+            if(keyeaten == 0){
+                i = 0;
+                break;
+            }
+            i += keyeaten;
+            if(!std::holds_alternative<std::string>(keyobj.inner)){
+                i = 0;
+                break;
+            }
+            if(json[i] == ':'){
+                i +=+ 1;
+            }
+            std::string key = std::move(std::get<std::string>(keyobj.inner));
+            auto [valobj, valeaten] = parse(json.substr(i));
+            if(valeaten == 0){
+                i = 0;
+                break;
+            }
+            i += valeaten;
+            res.try_emplace(std::move(key), std::move(valobj));
+            if(json[i] == ','){
+                i += 1;
+            }
+        }
+        return {JSONobject{std::move(res)}, i};
+    }
     return {JSONobject{std::nullptr_t{}}, 0};
 }
 
+template <class ...Fs>
+struct overloaded : Fs...{
+    using Fs::operator()...;
+};
 
+template <class ...Fs>
+overloaded(Fs...) -> overloaded<Fs...>;
 
 int main(){
-    std::string_view str3 = R"JSON([42,1,2,3,4])JSON";
+    std::string_view str3 = R"JSON({"work": 996, "school": [985, 211]}aaa)JSON";
     auto [obj, eaten] = parse(str3);
-    std::cout << eaten << std::endl;
+    auto const &dict = obj.get<JSONDict>();
+    print("The capitalist make me work in", dict.at("work").get<int>());
+    auto const &school = dict.at("school");
+    auto visitschool = [](auto const &school){
+        if constexpr(std::is_same_v<std::decay_t<decltype(school)>, JSONList>){
+            for(auto const &subschool : school){
+                print("I purchased my diploma from", subschool);
+            }
+        }else{
+            print("I Purchased my diploma from", school);
+        }
+    };
+    std::visit(overloaded{
+        [&](int val){
+           print("int is:", val);
+        },
+        [&](double val){
+           print("double is:", val);
+        },
+        [&](std::string val){
+           print("string is:", val);
+        },               
+        [&](auto val){
+            print("unknown object is:", val);
+        }
+    }, school.inner);
+    //for(auto const &school : schoollist){
+    //    print("I purchased my diploma from", school.get<int>());
+    // }
+    // std::cout << eaten << std::endl;
     // print(obj);
     return 0;
 }
